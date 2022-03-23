37c19f80fe355ffb71c06c40273918f1f5cabdf1
borteo <mattborgato@invisionapp.com>
2022-03-23 16:17:40 -0300

    add my site

---
+++ _data/sites/borgateo.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Borgateo.com", 
+	description: "My site gets tested every 23 hours",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.borgateo.com/"
+	]
+};
